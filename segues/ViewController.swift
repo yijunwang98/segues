d433d86a6348c4ee838635c44d20ce2d0d6e66d1
YJ <yw2859@alum.barnard.edu>
2022-07-14 07:54:16 -0700

    Initial Commit

---
+++ segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  segues
+//
+//  Created by scholar on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
