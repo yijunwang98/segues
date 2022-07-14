8b43bf10813858a8bb6c0d15333451146dc5882e
YJ <yw2859@alum.barnard.edu>
2022-07-14 11:22:41 -0700

    ok

---
+++ segues/secondScreen.swift
@@ -0,0 +1,29 @@
+//
+//  secondScreen.swift
+//  segues
+//
+//  Created by scholar on 7/14/22.
+//
+
+import UIKit
+
+class secondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
